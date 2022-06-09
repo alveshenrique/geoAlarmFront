f0d193e0eb97f3902f280cc0ab523b67ba027a90
alveshenrique <alveshenrique@ymail.com>
2022-06-09 11:17:51 -0300

    Create location component
    Added create-location component

--- geo-alarm-frontend/src/app/app.module.ts
+++ geo-alarm-frontend/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LocationListComponent } from './location-list/location-list.component';
+import { CreateLocationComponent } from './create-location/create-location.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LocationListComponent
+    LocationListComponent,
+    CreateLocationComponent
   ],
   imports: [
     BrowserModule,
