dd171a8796c95e9e4d816111da2d1fab507f950a
alveshenrique <alveshenrique@ymail.com>
2022-06-09 11:19:22 -0300

    create-location route
    Added route for the create-location component

--- geo-alarm-frontend/src/app/app-routing.module.ts
+++ geo-alarm-frontend/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateLocationComponent } from './create-location/create-location.component';
 import { LocationListComponent } from './location-list/location-list.component';
 
 const routes: Routes = [
   {path: 'locations', component: LocationListComponent},
+  {path: 'create-location', component: CreateLocationComponent},
   {path: '', redirectTo: 'locations', pathMatch: 'full'}
 ];
 
