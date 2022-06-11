c70463609b6fe062ae1ce717b24ba454fc8b1b53
alveshenrique <alveshenrique@ymail.com>
2022-06-11 17:45:21 -0300

    Added Geolocation
    Added geolocation using @ng-web-apis/geolocation

--- geo-alarm-frontend/src/app/location-details/location-details.component.ts
+++ geo-alarm-frontend/src/app/location-details/location-details.component.ts
@@ -3,6 +3,8 @@ import { ActivatedRoute } from '@angular/router';
 import { Location } from '../location';
 import { LocationService } from '../location.service';
 
+import {GeolocationService} from '@ng-web-apis/geolocation';
+
 @Component({
   selector: 'app-location-details',
   templateUrl: './location-details.component.html',
@@ -15,13 +17,23 @@ export class LocationDetailsComponent implements OnInit {
   clickLocation: Location = new Location();
 
   constructor(private route: ActivatedRoute,
-    private locationService: LocationService) { }
+    private locationService: LocationService,
+    private readonly geolocation: GeolocationService) { }
 
   ngOnInit(): void {
     this.id = this.route.snapshot.params[`id`];
 
     this.locationService.getLocationsById(this.id).subscribe({
       next: (data) => this.location = data,
+      error: (error) => console.log(error)
+    });
+
+    this.geolocation.subscribe({
+      next: (data: GeolocationPosition) => {
+        console.log(data);
+        this.clickLocation.latitude = data.coords.latitude;
+        this.clickLocation.longitude = data.coords.longitude;
+      },
       error: (error) => console.log(error)
     });
   }
