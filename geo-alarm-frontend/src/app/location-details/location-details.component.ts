321c4a24f7c878d021f0541e2aeae41e0ac323e2
alveshenrique <alveshenrique@ymail.com>
2022-06-11 12:12:47 -0300

    View Location finished
    View Location Details component finished and working

--- geo-alarm-frontend/src/app/location-details/location-details.component.ts
+++ geo-alarm-frontend/src/app/location-details/location-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '../location';
+import { LocationService } from '../location.service';
 
 @Component({
   selector: 'app-location-details',
@@ -7,9 +10,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LocationDetailsComponent implements OnInit {
 
-  constructor() { }
+  id: number;
+  location: Location;
+  constructor(private route: ActivatedRoute,
+    private locationService: LocationService) { }
 
   ngOnInit(): void {
+    this.id = this.route.snapshot.params[`id`];
+
+    this.locationService.getLocationsById(this.id).subscribe({
+      next: (data) => this.location = data,
+      error: (error) => console.log(error)
+    });
   }
 
 }
--- geo-alarm-frontend/src/app/location-list/location-list.component.ts
+++ geo-alarm-frontend/src/app/location-list/location-list.component.ts
@@ -64,7 +64,7 @@ export class LocationListComponent implements OnInit {
   }
 
   viewLocation(id: number) {
-    
+    this.router.navigate(['location-details', id]);
   }
 
 }
