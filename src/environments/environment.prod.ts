0a3fe859ffdd7ee546bf679d7ade635b1b431ee8
alveshenrique <alveshenrique@ymail.com>
2022-06-12 21:18:24 -0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 // HERE I'M GETTING MY API KEY FROM A FILE. REMOVE THIS LINE AND USE YOUR KEY INSTEAD OF API_KEY FROM env.keys FOR USAGE
-import {API_KEY} from '../environments/env.keys';
+// import {API_KEY} from '../environments/env.keys';
 
 export const environment = {
   production: true,
-  API_KEY: API_KEY
+  API_KEY: "API_KEY"
 };
